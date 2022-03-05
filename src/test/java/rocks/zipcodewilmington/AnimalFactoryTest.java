c1a6419cd58824f1f17c5c4c1244ecb646b1c493
Jason <jason@Zipcoders-MacBook-Pro.local>
2022-03-05 13:13:56 -0500

    factory test done

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,6 +1,14 @@
 package rocks.zipcodewilmington;
 
 import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Cat;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+
+import java.util.Date;
+
 /**
  * @author leon on 4/19/18.
  */
@@ -8,4 +16,34 @@ public class AnimalFactoryTest {
     //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
 
     //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+    // Given (cat data)
+
+
+    @Test
+        public void creatDogTest(){
+        String expectedName = "Milo";
+        Date expectedDate = new Date(1,1,1);
+
+        Dog newDog = AnimalFactory.createDog(expectedName,expectedDate);
+
+        String actualName = newDog.getName();
+        Date actualDate = newDog.getBirthDate();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+    }
+
+    @Test
+        public void createCatTest() {
+        String expectedName = "Zula";
+        Date expectedDate = new Date(1,1,1);
+
+        Cat newCat = AnimalFactory.createCat(expectedName, expectedDate);
+
+        String actualName = newCat.getName();
+        Date actualDate = newCat.getBirthDate();
+
+        Assert.assertEquals(expectedName, actualName);
+        Assert.assertEquals(expectedDate, actualDate);
+    }
 }
